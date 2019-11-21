917513f9ad9f1a3d5e84b20e9015360197f193b7
bhgs89 <han.baek.program@gmail.com>
2019-11-21 11:06:12 -0800

    Use Owl-Carousel-JS to create slides

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    margin: 5,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
+});
